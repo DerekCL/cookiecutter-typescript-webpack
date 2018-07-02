a17a20b1cc900f9d973871479a7deff7204e5ad4
Derek Lewandowski <dlewandowski@d-dlewandowski-mbp.local>
2018-07-02 13:17:42 -0500

    fixing webpack errors

--- {{cookiecutter.repo_name}}/config/webpack.dev.js
+++ {{cookiecutter.repo_name}}/config/webpack.dev.js
@@ -5,6 +5,7 @@ const common = require('./webpack.common');
 
 module.exports = merge(common, {
     devtool: 'eval-source-map',
+    mode: 'development',
     plugins: [
         new webpack.DefinePlugin({
             'process.env.NODE_ENV': JSON.stringify('development'),
--- {{cookiecutter.repo_name}}/config/webpack.prod.js
+++ {{cookiecutter.repo_name}}/config/webpack.prod.js
@@ -4,6 +4,7 @@ const webpack = require('webpack');
 const common = require('./webpack.common');
 
 module.exports = merge(common, {
+    mode: 'production',
     plugins: [
         new webpack.DefinePlugin({
             'process.env.NODE_ENV': JSON.stringify('production'),
